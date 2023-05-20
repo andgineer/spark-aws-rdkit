8632d35321cde6c6eeba2593bc6b68e9922e249d
andgineer <Andrei_Sorokin2@epam.com>
2023-05-20 06:13:44 +0200

    fixed submit bug health check improvement

--- src/health_check.py
+++ src/health_check.py
@@ -4,6 +4,7 @@ import sys
 
 
 LOCAL_SPARK = 'local'
+SHOW_RESULT_LENGTH = 5
 
 conf = pyspark.SparkConf()
 master_url = LOCAL_SPARK if len(sys.argv) <= 1 else sys.argv[1]
@@ -14,4 +15,5 @@ sc = SparkContext(conf=conf)
 big_list = range(10000)
 rdd = sc.parallelize(big_list, 2)
 odds = rdd.filter(lambda x: x % 2 != 0)
-print(f">>>>> Calculation finished: {odds.take(5)}")
+
+print(f">>>>> Calculation finished, {SHOW_RESULT_LENGTH} first odd numbers are {odds.take(SHOW_RESULT_LENGTH)}")
